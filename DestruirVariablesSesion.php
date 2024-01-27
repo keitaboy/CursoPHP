dbf9435dbd0def29aef316454825a50dc2e1dd51
Percy <pdaniel1255@gmail.com>
2024-01-27 12:11:51 -0500

    Clean

--- DestruirVariablesSesion.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-session_destroy();
-echo "Se destruyo la sesion de usuario"
-?>
--- VariablesSesion.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-$_SESSION["Usuario"]="Daniel";
-$_SESSION["Estado"]="Logueado";
-echo "Sesion Iniciada".":<br/>";
-echo "Usuario: ".$_SESSION["Usuario"]." se encuentra: ".$_SESSION["Estado"];
-?>
