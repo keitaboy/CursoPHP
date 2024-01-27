4126bbcb49bf5cc347954fa3f7a258e542d0f7ea
Percy <pdaniel1255@gmail.com>
2024-01-27 12:09:24 -0500

    Login v3

--- SistemaWeb/Login/index.php
+++ SistemaWeb/Login/index.php
@@ -34,24 +34,26 @@
 			<div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
 				<form class="login100-form validate-form">
 					<span class="login100-form-title p-b-49">
-						Login
+						INICIAR SESI&Oacute;N
 					</span>
 
 					<div class="wrap-input100 validate-input m-b-23" data-validate = "Username is reauired">
-						<span class="label-input100">Username</span>
-						<input class="input100" type="text" name="username" placeholder="Type your username">
+						<span class="label-input100">Usuario</span>
+						<input class="input100" type="text" name="username" placeholder="Escriba el nombre de usuario"
+						 id="txt_usu" autocomplete="new-password">
 						<span class="focus-input100" data-symbol="&#xf206;"></span>
 					</div>
 
 					<div class="wrap-input100 validate-input" data-validate="Password is required">
-						<span class="label-input100">Password</span>
-						<input class="input100" type="password" name="pass" placeholder="Type your password">
+						<span class="label-input100">Contrase&ntilde;a</span>
+						<input class="input100" type="password" name="pass" placeholder="Escriba la contrase&ntilde;a" 
+						id="txt_con">
 						<span class="focus-input100" data-symbol="&#xf190;"></span>
 					</div>
 					
 					<div class="text-right p-t-8 p-b-31">
 						<a href="#">
-							Forgot password?
+							Olvidaste la contrase&ntilde;a?
 						</a>
 					</div>
 					
@@ -59,16 +61,10 @@
 						<div class="wrap-login100-form-btn">
 							<div class="login100-form-bgbtn"></div>
 							<button class="login100-form-btn">
-								Login
+								ENTRAR
 							</button>
 						</div>
-					</div>
-
-					<div class="txt1 text-center p-t-54 p-b-20">
-						<span>
-							Or Sign Up Using
-						</span>
-					</div>
+					</div><br>
 
 					<div class="flex-c-m">
 						<a href="#" class="login100-social-item bg1">
@@ -83,16 +79,6 @@
 							<i class="fa fa-google"></i>
 						</a>
 					</div>
-
-					<div class="flex-col-c p-t-155">
-						<span class="txt1 p-b-17">
-							Or Sign Up Using
-						</span>
-
-						<a href="#" class="txt2">
-							Sign Up
-						</a>
-					</div>
 				</form>
 			</div>
 		</div>
@@ -100,7 +86,9 @@
 	
 
 	<div id="dropDownSelect1"></div>
-	
+
+<!--===============================================================================================-->
+	<script src="vendor/sweetalert2/sweetalert2.js"></script>	
 <!--===============================================================================================-->
 	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
 <!--===============================================================================================-->
@@ -119,4 +107,7 @@
 	<script src="js/main.js"></script>
 
 </body>
+<script>
+txt_usu.focus();
+</script>
 </html>
